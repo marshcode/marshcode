4f951df7c28584c0c0787efeb36fcb296913a4a5
David Marsh <davemarsh@marshcode.com>
2013-08-21 02:03:09 +0000

    commit of maze stuff.

--- src/davesite/app/factory.py
+++ src/davesite/app/factory.py
@@ -9,11 +9,13 @@ from flask import Flask
 
 from davesite.homepage import blueprint as homeblueprint
 from davesite.jlp import blueprint as jlprint
+from davesite.maze import blueprint as mazeprint
 
 def create_app(name):
     app = Flask(name)
     
     app.register_blueprint(homeblueprint.home_page)
     app.register_blueprint(jlprint.jlp, url_prefix="/jlp")
+    app.register_blueprint(mazeprint.maze, url_prefix="/maze")
     
     return app
