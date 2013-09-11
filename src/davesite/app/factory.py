36ba3a4a2a5b8e1ac0b525dc39926b36b4b53118
David Marsh <davemarsh@marshcode.com>
2013-09-11 00:18:29 +0000

    removed maze imports

--- src/davesite/app/factory.py
+++ src/davesite/app/factory.py
@@ -9,7 +9,6 @@ from flask import Flask
 
 from davesite.homepage import blueprint as homeblueprint
 from davesite.jlp import blueprint as jlprint
-from davesite.maze import blueprint as mazeprint
 
 from davesite.app import menu
 
@@ -20,7 +19,6 @@ def create_app(name):
     
     app.register_blueprint(homeblueprint.home_page)
     app.register_blueprint(jlprint.jlp, url_prefix="/jlp")
-    app.register_blueprint(mazeprint.maze, url_prefix="/maze")
         
     menu_items = menu.read_menu()
     @app.context_processor
