bc76db96efdb0d21137b2177e1a801b5470637e6
David Marsh <davemarsh@marshcode.com>
2013-10-06 23:12:23 +0000

    documentation added to the default_config

--- src/davesite/app/default_config.py
+++ src/davesite/app/default_config.py
@@ -1,4 +1,12 @@
+#enable flask debug mode.  If this is active, the python logging system will not be configured
 DEBUG=False
+
+#Log file containing warnings and above.  It is reccomended that this be an absolute path in the second config file
 ERROR_LOG_FILE = "davesite.error.log"
-MENU = []       
-SCRIPT_NAME='/'        
+
+#Nested list with entries of the format ('Heading', 'http://www.the_url.com') or
+#('Menu Heading', [('Heading1', 'URL1'), ('Heading2', 'URL2')])
+MENU = []                               
+                                                                              
+#Script location on the server (relative to the root).  Use this to adjust the prefix created by flask.url_for
+SCRIPT_NAME='/'                            
